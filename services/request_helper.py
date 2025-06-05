e784f295e72c34f4f7ce0884edcf9715f09dc774
Gustavo Martins <gucmartins@gmail.com>
2025-06-05 09:09:52 -0300

    Resquest helper bugfix and refactor

--- services/request_helper.py
+++ services/request_helper.py
@@ -22,40 +22,51 @@ def run_http_request(method: str, url: str, headers: dict, cookies: dict, data: 
             print(f"Unsupported HTTP method: '{method}'.")
 
     except requests.RequestException as e:
-        print(f"Erro na requisição: {e}")
+        print(f"Request error: {e}")
 
 
 def parse_curl_to_requests(curl_command: str) -> dict:
     curl_command = " ".join(curl_command.strip().splitlines())
+    return {
+        'method': extract_http_method(curl_command),
+        'url': extract_url(curl_command),
+        'headers': extract_headers(curl_command),
+        'cookies': extract_cookies(curl_command),
+        'payload': extract_payload(curl_command)
+    }
 
-    headers = extract_headers(curl_command)
 
+def parse_headers_to_requests(curl_command: str) -> dict:
+    curl_command = " ".join(curl_command.strip().splitlines())
+    return {
+        'headers': extract_headers(curl_command),
+        'cookies': extract_cookies(curl_command)
+    }
+
+
+def extract_http_method(curl_command):
     method_match = re.search(r"-X (\w+)", curl_command)
     method = method_match.group(1).upper() if method_match else ("POST" if "--data" in curl_command else "GET")
+    return method
 
+
+def extract_url(curl_command):
     url_pattern = r"curl '([^']+)'"
     url_match = re.search(url_pattern, curl_command)
     url = url_match.group(1) if url_match else None
+    return url
 
+
+def extract_payload(curl_command):
     payload_match = re.search(r"--data(?:-raw|-binary)? '([^']+)'", curl_command)
     raw_data = payload_match.group(1) if payload_match else None
-
     payload = None
     if raw_data:
         try:
             payload = json.loads(raw_data)
         except json.JSONDecodeError:
             pass
-
-    cookies = extract_cookies(curl_command)
-
-    return {
-        'method': method,
-        'url': url,
-        'headers': headers,
-        'cookies': cookies,
-        'payload': payload
-    }
+    return payload
 
 
 def extract_cookies(curl_command):
@@ -71,13 +82,3 @@ def extract_headers(curl_command):
     headers_pattern = r"-H '([^:]+): ([^']+)'"
     headers = dict(re.findall(headers_pattern, curl_command))
     return headers
-
-
-def parse_headers_to_requests(curl_command: str) -> dict:
-    headers = extract_headers(curl_command)
-    cookies = extract_headers(curl_command)
-
-    return {
-        'headers': headers,
-        'cookies': cookies
-    }
